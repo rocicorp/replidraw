ba401f69e06b87356bc0f5acdbace0f31bec3462
Aaron Boodman <aaron@aaronboodman.com>
2021-09-20 20:07:40 -1000

    Use RWLock from Replicache rather than built-in tx support

--- worker/src/kv.ts
+++ worker/src/kv.ts
@@ -1,4 +1,9 @@
-import { Store, Read, Write, Value } from './replicache/src/kv/store'
+import { Store, Read, Write, Value } from './replicache/src/kv/store';
+import {
+  deleteSentinel,
+  WriteImplBase,
+} from './replicache/src/kv/write-impl-base';
+import { RWLock } from './replicache/src/rw-lock';
 
 // Implements the Replicache kv.Store interface in terms of Durable Objects.
 // This is nice because theoretically, we get the entire Replicache userland
@@ -6,72 +11,83 @@ import { Store, Read, Write, Value } from './replicache/src/kv/store'
 //
 // In practice, we can't quite get there yet. See comments in TransactionImpl.
 export class StoreImpl implements Store {
-  private _state: DurableObjectState
+  private _state: DurableObjectState;
+  private _lock = new RWLock();
 
   constructor(state: DurableObjectState) {
-    this._state = state
+    this._state = state;
   }
 
   async withRead<R>(f: (read: Read) => R | Promise<R>): Promise<R> {
-    // TODO: Durable Objects doesn't distinguish between read and write
-    // transactions. Does the fact that it's doing optimistic concurrency
-    // mean that there's no perf difference, or do we need to do something
-    // clever so that reads don't get an exclusive lock.
-    return await this.withWrite(f)
+    let read;
+    try {
+      read = await this.read();
+      return await f(read);
+    } finally {
+      read?.release();
+    }
   }
 
   async withWrite<R>(f: (write: Write) => R | Promise<R>): Promise<R> {
-    let r: R
-    await this._state.storage.transaction(async tx => {
-      r = await f(new WriteImpl(tx))
-    })
-    return r!
+    let write;
+    try {
+      write = await this.write();
+      const rv = await f(write);
+      await write.commit();
+      return rv;
+    } finally {
+      write?.release();
+    }
   }
 
   async close(): Promise<void> {}
 
   async read(): Promise<Read> {
-    // can be implemented with resolvers, but not sure we need it.
-    throw new Error('Method not implemented.')
+    const release = await this._lock.read();
+    return new ReadImpl(this._state.storage, release);
   }
 
-  write(): Promise<Write> {
-    throw new Error('Method not implemented.')
+  async write(): Promise<Write> {
+    const release = await this._lock.write();
+    return new WriteImpl(this._state.storage, release);
   }
 }
 
-class WriteImpl implements Write {
-  private _tx: DurableObjectTransaction
-  private _didCommit: boolean
+class ReadImpl implements Read {
+  private _storage: DurableObjectOperator;
+  readonly release: () => void;
 
-  constructor(tx: DurableObjectTransaction) {
-    this._tx = tx
-    this._didCommit = false
+  constructor(storage: DurableObjectOperator, release: () => void) {
+    this._storage = storage;
+    this.release = release;
   }
 
   async has(key: string): Promise<boolean> {
-    return (await this._tx.get(key)) !== undefined
+    return (await this._storage.get(key)) !== undefined;
   }
 
   async get(key: string): Promise<Value | undefined> {
-    return await this._tx.get(key)
+    return await this._storage.get(key);
   }
+}
 
-  async put(key: string, value: Value): Promise<void> {
-    await this._tx.put(key, value)
-  }
+class WriteImpl extends WriteImplBase {
+  private _storage: DurableObjectOperator;
 
-  async del(key: string): Promise<void> {
-    await this._tx.delete(key)
+  constructor(storage: DurableObjectOperator, release: () => void) {
+    super(new ReadImpl(storage, release));
+    this._storage = storage;
   }
 
   async commit(): Promise<void> {
-    this._didCommit = true
-  }
-
-  release(): void {
-    if (!this._didCommit) {
-      this._tx.rollback()
-    }
+    await Promise.all(
+      [...this._pending].map(async ([k, v]) => {
+        if (v === deleteSentinel) {
+          await this._storage.delete(k);
+        } else {
+          await this._storage.put(k, v);
+        }
+      }),
+    );
   }
 }
