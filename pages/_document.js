8478f58fced546c64e05bfe3cf182025c135a018
Aaron Boodman <aaron@aaronboodman.com>
2021-11-12 11:47:26 -1000

    Fix crossorigin warning

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
             integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
