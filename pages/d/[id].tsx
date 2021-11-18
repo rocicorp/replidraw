15cd7e3a7184326514faaf0a393e116be095f9cb
Aaron Boodman <aaron@aaronboodman.com>
2021-11-17 21:41:44 -1000

    Implement frontend

--- pages/d/[id].tsx
+++ pages/d/[id].tsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
-import { HTTPRequestInfo, PullerResult, Replicache } from "replicache";
+import { Replicache } from "replicache";
 import { Designer } from "../../frontend/designer";
 import { Nav } from "../../frontend/nav";
 import { M, mutators } from "../../frontend/mutators";
 import { randUserInfo } from "../../frontend/client-state";
 import { randomShape } from "../../frontend/shape";
-import { Request, responseSchema } from "schemas/socket";
-import { PushRequest, PushResponse } from "schemas/push";
-import { PullRequest, PullResponse } from "schemas/pull";
+import { Request, responseSchema } from "schemas/network";
+import { PushRequest } from "schemas/push";
 import { resolver } from "frontend/resolver";
-import { nanoid } from "nanoid";
+import { PokeResponse } from "schemas/poke";
 
 export default function Home() {
   const [rep, setRep] = useState<Replicache<M> | null>(null);
@@ -27,74 +26,23 @@ export default function Home() {
         name: docID,
         mutators,
 
-        // These pusher and puller implementations are a bit hacky for the moment.
-        // Even though the core replicache protocol is very asynchronous and doesn't
-        // care about replies, there are two exceptions:
-        //
-        // - The connection loop wants to know when a response is done so that it can
-        //   do exponential backoff.
-        // - The puller needs to know the response to pull because it needs to enforce
-        //   that pulls are serialized.
-        //
-        // When we fix both of these things in Replicache, this will get a lot simpler
-        // because the responses will go away.
-
         pusher: async (req) => {
           const ws = await socket;
-          const pushReq = (await req.json()) as PushRequest;
-          pushReq.id = nanoid();
-          const msg: Request = ["pushReq", pushReq];
-
-          const { promise, resolve } = resolver<HTTPRequestInfo>();
-          const listener = (e: MessageEvent) => {
-            const data = JSON.parse(e.data);
-            const [type, res] = responseSchema.parse(data);
-            if (type == "pushRes") {
-              const pushRes = res as PushResponse;
-              if (pushRes.id == pushReq.id) {
-                ws.removeEventListener("message", listener);
-                resolve({
-                  errorMessage: "",
-                  httpStatusCode: 200,
-                });
-              }
-            }
-          };
-
-          ws.addEventListener("message", listener);
+          const push = (await req.json()) as PushRequest;
+          const msg: Request = ["push", push];
           ws.send(JSON.stringify(msg));
-          return await promise;
+          return {
+            errorMessage: "",
+            httpStatusCode: 200,
+          };
         },
 
-        puller: async (req) => {
-          const ws = await socket;
-          const pullReq = (await req.json()) as PullRequest;
-          const msg: Request = ["pullReq", pullReq];
-
-          const { promise, resolve } = resolver<PullerResult>();
-          const listener = (e: MessageEvent) => {
-            const data = JSON.parse(e.data);
-            const [type, res] = responseSchema.parse(data);
-            if (type == "pullRes") {
-              const pullRes = res as PullResponse;
-              if (pullRes.baseCookie == pullReq.cookie) {
-                ws.removeEventListener("message", listener);
-                resolve({
-                  httpRequestInfo: {
-                    errorMessage: "",
-                    httpStatusCode: 200,
-                  },
-                  response: pullRes,
-                });
-              }
-            }
-          };
-
-          ws.addEventListener("message", listener);
-          ws.send(JSON.stringify(msg));
-
-          return await promise;
+        requestOptions: {
+          minDelayMs: 50,
         },
+
+        // No puller: unneeded with poke
+        // We should remove from Replicache?
       });
 
       const socket = (async () => {
@@ -106,11 +54,27 @@ export default function Home() {
         ws.addEventListener("open", () => {
           resolve(ws);
         });
-        ws.addEventListener("message", (e) => {
+
+        let gotFirstMessage = false;
+        ws.addEventListener("message", async (e) => {
           const data = JSON.parse(e.data);
-          const [type] = responseSchema.parse(data);
-          if (type == "pokeRes") {
-            r.pull();
+          const [type, res] = responseSchema.parse(data);
+          if (type == "poke") {
+            const pr = res as PokeResponse;
+            await r.poke({
+              baseCookie: pr.baseCookie,
+              pullResponse: {
+                lastMutationID: pr.lastMutationID,
+                cookie: pr.cookie,
+                patch: pr.patch,
+              },
+            });
+            if (!gotFirstMessage) {
+              gotFirstMessage = true;
+              r.mutate.initShapes(
+                Array.from({ length: 5 }, () => randomShape())
+              );
+            }
           }
         });
         return await promise;
@@ -121,12 +85,6 @@ export default function Home() {
         id: await r.clientID,
         defaultUserInfo,
       });
-      r.onSync = (syncing: boolean) => {
-        if (!syncing) {
-          r.onSync = null;
-          r.mutate.initShapes(Array.from({ length: 5 }, () => randomShape()));
-        }
-      };
 
       setRep(r);
     })();
