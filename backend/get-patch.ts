a4f270a91ebb1b60dac1756805c3fb7c535e45f1
Aaron Boodman <aaron@aaronboodman.com>
2021-11-17 21:34:17 -1000

    Introduce getPatch()

---
+++ backend/get-patch.ts
@@ -0,0 +1,33 @@
+import { Patch, Cookie } from "schemas/poke";
+import { Executor } from "./db";
+
+export async function getPatch(
+  executor: Executor,
+  docID: string,
+  fromCookie: Cookie
+): Promise<Patch> {
+  const t0 = Date.now();
+  const entries = await executor(
+    `select k, v, deleted from object where documentid = $1 and version > $2`,
+    [docID, fromCookie ?? 0]
+  );
+  console.log(`Read ${entries.rows.length} objects in`, Date.now() - t0);
+
+  const patch: Patch = [];
+  for (let row of entries.rows) {
+    const { k, v, deleted } = row;
+    if (deleted) {
+      patch.push({
+        op: "del",
+        key: k,
+      });
+    } else {
+      patch.push({
+        op: "put",
+        key: k,
+        value: JSON.parse(v),
+      });
+    }
+  }
+  return patch;
+}
