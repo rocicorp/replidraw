14b9038b6d8114e0c636fa6702a7a85f9e3cdf69
Erik Arvidsson <erik.arvidsson@gmail.com>
2022-05-09 23:42:49 +0200

    fix: Fix modals (#81)
    When transitioning to the Todo backend a bunch of files got
    deleted/changed.
    
    We are using bootstrap for the modal dialogs and that got left out.
    
    Instead of doing <link> for the bootstrap css I'm now using webpack
    import css.
    
    Followup to 2a96d562a2d6a355357b52743cb73e37c55afbad

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "todomvc-app-css/index.css";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
